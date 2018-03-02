9ec43187013d9041d5bf7dfe6ac7ec5043015274
Geovane Menezes <geovane.menezes@seati.ma.gov.br>
2018-03-02 15:30:50 -0300

    Adicionando codigo que transforma as imagens em realcadas e 3x menores além da base obtida através da aplicação deste

--- codigos/realcar.py
+++ codigos/realcar.py
@@ -1,24 +1,56 @@
 import cv2
 import numpy as np
+import os
+
+folder = "/home/geovane/artigoRITA/base/base_simara_sem_melhoria"
+folder_to_save = "/home/geovane/artigoRITA/base/base_simara_realce_log"
 
 def realce_logaritmico(img):
 	l = np.log10(img.astype(np.uint16)+1)
 	return (l*105.9612).astype(np.uint8)
 	
 if __name__ == '__main__':
-	img = cv2.imread('/home/geovane/Imagens/teste.png', 0)
-	cv2.imshow('img', img)
-	
-	print(img.max(), img.min())
-	
-	realcada = realce_logaritmico(img)
-	
-	print(realcada.max(), realcada.min())
-	
-	cv2.imshow('realce', realcada)
-	
-	cv2.waitKey()
-	
-# PEGAR O CODIGO QUE LE AS IMAGENS, APLICAR O REALCE NELAS
+
+	for c in os.listdir(folder):
+		
+		class_path = folder + '/' + c
+		
+		print(class_path)
+		
+		# Percorrendo todos os individuos presentes na classe
+		for f in os.listdir(class_path):
+		
+			file_name = class_path + '/' + f
+
+			print('> Reading:', file_name)
+			
+			sample = open(file_name)
+			
+			rows, cols = sample.readline().split('\t')
+			
+			rows, cols = int(rows), int(cols)
+			
+			img = np.ndarray((cols, rows), np.uint8)
+			
+			# Lendo os valores
+			for x in list(range(cols)):
+				for y in list(range(rows)):
+					value = int(sample.readline())
+					
+					if (value < 0):
+						img[x][y] = 0
+					else:
+						img[x][y] = value
+
+			img = cv2.resize(img, None, fx=1/3, fy=1/3)
+
+			realce = realce_logaritmico(img)
+
+			filename_to_save = folder_to_save + '/' + c + '/' + f.split('.')[0] + '.jpg'
+
+			print(filename_to_save)
+
+			cv2.imwrite(filename_to_save, realce)
+
 # GERAR OS HISTOGRAMAS SEM ABORDAGENS DE DIVISÃO ESPACIAL COM AS NOVAS IMAGENS REALCADAS (PRA VER SE CHEGAM NA CASA DOS 80 DE F-SCORE)
 # TESTAR TAMBEM FAZER O HISTOGRAMA MANUALMENTE, COMO EU FIZ DA VEZ QUE TESTEI ISSO
